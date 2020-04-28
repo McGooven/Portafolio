b56f505bd7cf99056953df0058c89d9f193727d7
robertoMayol <r.mayol@alumnos.duoc.cl>
2020-04-28 11:57:46 -0400

    MERGE DESDE MASTER

--- Desktop/IntegritySoftware/src/controladores/AdvRol/PacienteFormController.java
+++ Desktop/IntegritySoftware/src/controladores/AdvRol/PacienteFormController.java
@@ -3,35 +3,34 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controladores;
+package controladores.AdvRol;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.ComboBoxBase;
-import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
-
-
 /**
  * FXML Controller class
  *
  * @author rober
  */
-public class CrearUsuarioController implements Initializable {
-    @FXML TextField txtUsuario, txtCorreo;
-    @FXML PasswordField pswContrasenia;
-    @FXML ComboBoxBase cmbTipoUsuario;
-    @FXML Button btnAceptar;
-    @FXML ImageView imgLogo;    /**
+public class PacienteFormController implements Initializable {
+
+    /**
      * Initializes the controller class.
+     * 
      */
+    @FXML  TextField txtCorreo, txtIdFicha, txtEtapa, txtPrimerNombre, txtSegundoNombre, txtPrimerApellido, txtSegundoApellido, txtRut, txtCalle;
+    @FXML  ComboBox cmbCentro, cmbRegion, cmbComuna;
+    @FXML  DatePicker dmpFechaNacimiento;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
 
     }    
     
