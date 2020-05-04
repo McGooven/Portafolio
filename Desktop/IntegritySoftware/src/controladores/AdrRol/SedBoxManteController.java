618fe08551006042b48a19bce2c37866df1d4551
GaidenZig <d.s.r.asymmetric@gmail.com>
2020-05-04 19:23:13 -0400

    administrador

--- Desktop/IntegritySoftware/src/controladores/AdrRol/SedBoxManteController.java
+++ Desktop/IntegritySoftware/src/controladores/AdrRol/SedBoxManteController.java
@@ -11,7 +11,13 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import controladores.PeticionJSON;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.TableColumn;
+import javafx.scene.input.MouseEvent;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
@@ -22,15 +28,25 @@ import org.json.JSONObject;
 public class SedBoxManteController implements Initializable {
     StageController stageController;
     
-    @FXML private ListView<?> ltvSedes;
+    @FXML private ListView<String> ltvSedes;
     @FXML private Button btnConfirmar,btnLimpiar,btnCrearSede,btnEditarSede;
-    @FXML private TableView<?> tbvBoxesSede;
-    @FXML private TableColumn<?, ?> colId, colEstado, colHabilitada;
+    @FXML private TableView<Box> tbvBoxesSede;
+    @FXML private TableColumn<Box, Integer> colId; 
+    @FXML private TableColumn<Box, String> colEstado; 
+    @FXML private TableColumn<Box, String> colHabilitada;
     @FXML private Pane panSedeCardContainer;
     @FXML private TextField txtFRegSede,txtFCiuSede,txtFCalleSede,txtFAnomSede;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        PeticionJSON request = new PeticionJSON(new JSONObject(), "get", "http://localhost:3000/api/centros"); 
+        request.connect();
+        
+        JSONArray boxes = request.res.getJSONObject(0).getJSONArray("centro");
+        
+        ObservableList<Box> list = FXCollections.observableArrayList(
+                
+        );
         
     }
     
@@ -40,6 +56,44 @@ public class SedBoxManteController implements Initializable {
      */
     public void setStageController(StageController c){
         this.stageController = c;
+        
+    }
+    
+    public static class Box{
+        private SimpleIntegerProperty idBox;
+        private SimpleStringProperty habilitada;
+        private SimpleStringProperty estado;
+        
+        public Box(Integer idBox, String habilitada, String estado) {
+            this.idBox = new SimpleIntegerProperty(idBox);
+            this.habilitada = new SimpleStringProperty(habilitada);
+            this.estado = new SimpleStringProperty(estado);
+        }
+
+        public int getIdBox() {
+            return idBox.get();
+        }
+
+        public void setIdBox(int idBox) {
+            this.idBox.set(idBox);
+        }
+
+        public String getHabilitada() {
+            return habilitada.get();
+        }
+
+        public void setHabilitada(String habilitada) {
+            this.habilitada.set(habilitada);
+        }
+
+        public String getEstado() {
+            return estado.get();
+        }
+
+        public void setEstado(String estado) {
+            this.estado.set(estado);
+        }
+        
     }
     
 }
